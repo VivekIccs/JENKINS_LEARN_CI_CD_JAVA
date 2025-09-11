a09a447c924cc794f1928d5526d7da9fcc104238
Yadav <viyadav@ptc.com>
2025-09-11 08:48:57 +0530

    test change

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("CI CD Try kar raha hu Jenkins ka use karke");
-        System.out.println("Aj date hai : " + new Date());
+        System.out.println("Aj date hai = " + new Date());
         System.out.println("Pahla program compile and run ho gya hurray!");
     }
 
