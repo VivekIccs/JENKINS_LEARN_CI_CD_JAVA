bd177383e124a3efb701b985a9dc16cb97cd4ca8
Yadav <viyadav@ptc.com>
2025-09-12 07:28:40 +0530

    test

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("CI CD Try kar raha hu Jenkins ka use karke");
         System.out.println("Aj date hai = " + new Date());
-        System.out.println("Pahla program compile and run ho gya hurray!");
+        System.out.println("Pahla program compile and run ho gya hurray");
     }
 
 }
